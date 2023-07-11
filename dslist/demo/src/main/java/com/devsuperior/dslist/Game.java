a181c6f5b1f45277188698c53d48bbc12f3e7f56
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2023-07-11 07:58:57 -0300

    Populado o banco de dados. Adicionado Pluggin Maven

--- dslist/demo/src/main/java/com/devsuperior/dslist/Game.java
+++ dslist/demo/src/main/java/com/devsuperior/dslist/Game.java
@@ -17,18 +17,23 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
-    public Game(Long id, String title, Integer year, String genre, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre,String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -90,12 +95,20 @@ public class Game {
         this.longDescription = longDescription;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     @Override
